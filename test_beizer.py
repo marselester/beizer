3aec8259a53dacfe7533f82a9d13bc3502386039
marselester <marselester@ya.ru>
2012-06-30 10:21:22 +0600

    Added description of matrix initialization tests

---
+++ test_beizer.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from beizer.models import TransitionMatrix
+
+
+class MatrixInitTest(unittest.TestCase):
+    def test_init_takes_at_least_two_arguments(self):
+        pass
+
+    def test_argument_is_not_list(self):
+        pass
+
+    def test_empty_list(self):
+        pass
+
+    def test_matrix_is_not_quadratic(self):
+        pass
+
+    def test_matrix_has_only_source(self):
+        pass
+
+    def test_matrix_has_only_source_and_drain(self):
+        pass
+
+    def test_matrix_has_source_and_drain_and_loop(self):
+        pass
+
+    def test_matrix_has_one_loop_and_seven_transitions(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
