37eb248928ea226e7c9096ae7f89fdf1948d0eec
marselester <marselester@ya.ru>
2012-06-30 14:17:36 +0600

    Added base exception class and class of matrix init errors

---
+++ beizer/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+class BeizerException(Exception):
+    """Base exception class of beizer module."""
+
+
+class MatrixInitError(BeizerException):
+    """Errors of matrix initialization."""
+
