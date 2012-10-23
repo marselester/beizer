c1351ecabc279607c656a1f747e53d971630b2cd
marselester <marselester@ya.ru>
2012-10-23 14:31:55 +0600

    Added author, author_email to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from distutils.core import setup
 setup(
     name='beizer',
     version='0.1dev',
-    packages=['beizer']
+    packages=['beizer'],
+    author='marselester',
+    author_email='marselester@ya.ru'
 )
